e0eb3fc9a12ff4042f078c5d346ddbb670f2518e
chloemaillot <hello@chloemaillot.fr>
2025-06-01 13:52:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -105,3 +105,8 @@ def elevenlabs_speak(text, user_id="output"):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Yippee backend is running 🚀"
+
